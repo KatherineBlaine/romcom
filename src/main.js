521e139c400f76b12111f296d7c0974d8c9d27f9
Chrissy Cooper <cooper.christine.r@gmail.com>
2022-12-08 22:53:10 -0600

    Update variables and add pseudocode

--- src/main.js
+++ src/main.js
@@ -1,17 +1,12 @@
 // Create variables targetting the relevant DOM elements here 👇
 
-var myTitle = document.querySelector(".cover-title");
-var myTagLine1 = document.querySelector(".tagline-1");
-var myTagLine2 = document.querySelector(".tagline-2");
-var myImage = document.querySelector(".cover-image");
+var defaultTitle = document.querySelector(".cover-title");     //sets the cover-title html by class to a variable
+var defaultTagLine1 = document.querySelector(".tagline-1");
+var defaultTagLine2 = document.querySelector(".tagline-2");
+var defaultImage = document.querySelector(".cover-image");
 var randomCoverButton = document.querySelector('.random-cover-button');
-var formViewButton = document.querySelector('.make-new-button')
-console.log(formViewButton)
-var homeButton = document.querySelector('.home-button') 
-//console.log(homeButton)
-
-var formView = document.querySelector('.view form-view hidden')
-//formView.style.display = 'block'
+var formViewButton = document.querySelector('.make-new-button');
+var homeButton = document.querySelector('.home-button');
 
 // We've provided a few variables below
 var savedCovers = [
@@ -20,53 +15,45 @@ var savedCovers = [
 var currentCover;
 
 // Add your event listeners here 👇
-//on click - create a new random instance of the cover class
 
-
-randomCoverButton.addEventListener('click', function() {
+randomCoverButton.addEventListener('click', function() {          //sets EL to the random cover button, creates new cover, and displays it
   createNewCover()
   displayNewCover(currentCover)}); 
-window.addEventListener('load', function() {
+
+window.addEventListener('load', function() {                      //sets EL to window, on load, creates new cover and displays it
   createNewCover()
   displayNewCover(currentCover)});
 
   formViewButton.addEventListener('click', function(){
-    document.querySelector('.home-view').hidden = true;
-    document.querySelector('.home-button').hidden = false;
-    document.querySelector('.form-view').hidden = false;
-    
-  }, false)
-console.log(document.querySelector('.home-view'))
+    document.querySelector('.home-view').hidden = true;           //this uses the .hidden property to hide the homeview 
+    document.querySelector('.form-view').style.display = "block"; //this uses the .style.display properties to show the form view
+    document.querySelector('.home-button').hidden = false;        //we should implement the above here
+    //hide show new cover button with .hidden?
+    //hide save cover button with .hidden?
+  })
+
+  //apply above to when user clicks "save new cover button"
+    //saved covers section should be visible ("block")
+    //homepage should be hidden (copy line 30)
+    //hide show new random cover button
+    //hide save cover button
+
 
 // Create your event handlers and other functions here 👇
 function createNewCover() {
-    var randomDescriptor1 = descriptors[getRandomIndex(descriptors)];
+    var randomDescriptor1 = descriptors[getRandomIndex(descriptors)];         //uses getRandomIndex to generate index value for data.js arrays
     var randomDescriptor2 = descriptors[getRandomIndex(descriptors)];
     var randomCover = covers[getRandomIndex(covers)];
     var randomTitle = titles[getRandomIndex(titles)];
   currentCover = new Cover(randomCover, randomTitle, randomDescriptor1, randomDescriptor2);
 };
 
-function displayNewCover(currentCover) {
-  myTitle.innerText = currentCover.title;
-  myTagLine1.innerText = currentCover.tagline1;
-  myTagLine2.innerText = currentCover.tagline2;
-  myImage.src = currentCover.cover;
+function displayNewCover(currentCover) {                            //sets default main-cover html info to our randomly generated cover
+  defaultTitle.innerText = currentCover.title;
+  defaultTagLine1.innerText = currentCover.tagline1;
+  defaultTagLine2.innerText = currentCover.tagline2;
+  defaultImage.src = currentCover.cover;
 }
-
-function changeToFormView(){
-  //show the home button
-  homeButton
-    //change the .innerHTML() to view form - view hidden
-  //show the form view
-    //change the .innerHTML to view form - view hidden
-  //hide the home view
-  //hide save cover button
-  //hide show new random cover button
-}
-
-changeToFormView()
-
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
